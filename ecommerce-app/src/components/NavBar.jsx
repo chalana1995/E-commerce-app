6721b0ba01666c956bfc1355cbda506f3ab08a48
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-10-07 21:30:31 +0530

    finish 1st styleing part

--- ecommerce-app/src/components/NavBar.jsx
+++ ecommerce-app/src/components/NavBar.jsx
@@ -1,7 +1,59 @@
+import { Search } from "@material-ui/icons";
 import React from "react";
+import styled from "styled-components";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            Input
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default NavBar;
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+`;
+
+const Center = styled.div`
+  flex: 1;
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
